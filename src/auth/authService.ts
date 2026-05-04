ba8fe94fb21e1fc965b2760620e8fb7095e257a4
thiago perrig <thiago perrig>
2026-05-04 11:09:51 -0300

    Implementé auths para login

---
+++ src/auth/authService.ts
@@ -0,0 +1,38 @@
+import {  supabase } from "../config/bd"
+import  jwt  from "jsonwebtoken"
+
+const SECRET = "seguro"
+
+export const AuthService = {
+    async login(email: string, password: string) {
+
+        const { data: user, error } = await supabase
+            .from("usuarios")
+            .select("*")
+            .eq("email", email)
+            .single()
+
+        if (error || !user) {
+            throw new Error("Usuario no encontrado")
+        }
+
+        if (user.password !== password) {
+            throw new Error("Contraseña incorrecta")
+        }
+
+        const token = jwt.sign(
+            {
+                id: user.id,
+                rol: user.rol
+            },
+            SECRET,
+            { expiresIn: "2h"}
+        )
+
+        return {
+            token,
+            rol: user.rol,
+            nombre: user.nombre
+        }
+    }
+}
